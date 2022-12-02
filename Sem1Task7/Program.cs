0371f1ef5dab5521ccc21df38d94a8c342af7fc3
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-02 19:24:58 +0300

    Task 7 completed

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,8 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine!=null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int outDigit = inputNumber % 10;
+    Console.WriteLine(outDigit);
+}
