b340c5c6caf1fbe79cdbe9971c1a6f40fb6e38f5
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-03 00:54:33 +0300

    Comments added for Task 6

--- Sem1Task6/Program.cs
+++ Sem1Task6/Program.cs
@@ -1,15 +1,22 @@
 ﻿Console.WriteLine("Введите число:");
+//read number
 string? inputLine = Console.ReadLine();
+//null safety check
 if(inputLine!=null)
 {
+    //convert string into integer
     int number = int.Parse(inputLine);
+    //check if number is devisible by 2
     int remainder = number%2;
+    //define if number is even or odd
     if (remainder==0)
     {
+        //output result if number is even
         Console.WriteLine("Данное число является четным.");
     }
     else
     {
+        //output result if number is odd
         Console.WriteLine("Данное число не является четным.");
     }
 }
