1e1e1c13f3d62a7984ef5ae5705327e33add3114
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-03 00:34:05 +0300

    Comments added for Task 2

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -1,24 +1,33 @@
 ﻿Console.WriteLine("Введите первое число:");
-string? inputLine1 = Console.ReadLine();
+//read first number
+string? inputLine1 = Console.ReadLine();                                               
 Console.WriteLine("Введите второе число:");
+//read second number
 string? inputLine2 = Console.ReadLine();
-if(inputLine1!=null && inputLine2!=null)
+//null safety check                                              
+if(inputLine1!=null && inputLine2!=null)                                               
 {
-    int Number1 = int.Parse(inputLine1);
-    int Number2 = int.Parse(inputLine2);
-    if(Number1 > Number2)
+    //convert string into ineger
+    int number1 = int.Parse(inputLine1);
+    int number2 = int.Parse(inputLine2);
+    //check if first number is greater than second
+    if(number1 > number2)
     {
-        Console.WriteLine("Число " + Number1 + " больше числа " + Number2);
+        //output result of check
+        Console.WriteLine("Число " + number1 + " больше числа " + number2);
     }
     else
     {
-        if(Number1 == Number2)
+        //check if first number and second number are equal
+        if(number1 == number2)
         {
-            Console.WriteLine("Число " + Number2 + " равно числу " + Number1);
+            //output result of check
+            Console.WriteLine("Число " + number2 + " равно числу " + number1);
         }
         else
         {
-            Console.WriteLine("Число " + Number2 + " больше числа " + Number1);
+            //output result of check
+            Console.WriteLine("Число " + number2 + " больше числа " + number1);
         }
     }
 }
