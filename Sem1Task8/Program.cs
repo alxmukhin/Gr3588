34747ae3c86449fa38a09685829af178881c112f
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-03 00:25:54 +0300

    Task 8 completed

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число:");
+string? inputLine = Console.ReadLine();
+if(inputLine!=null)
+{
+     int number = int.Parse(inputLine);
+     int index = 2;
+     string outLine = string.Empty;
+     if(number%2 == 0)
+     {
+        while(index <= number)
+        {
+            outLine = outLine + index + "  ";
+            index = index + 2;
+        }
+     }
+     else
+     {
+         while(index < number)
+        {
+            outLine = outLine + index + "  ";
+            index = index + 2;
+        }
+     }
+     Console.WriteLine(outLine);
+}
