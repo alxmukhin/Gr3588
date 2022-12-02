f420fffa556f958c38038e0ede6cc0499f9dbc56
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-02 17:55:05 +0300

    Task 1 completed

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// read two numbers
+string? Num1Line = Console.ReadLine();
+string? Num2Line = Console.ReadLine();
+
+// check if these numbers are not equal to null
+if(Num1Line!=null&&Num2Line!=null)
+{
+    // convert strint into integer
+    int num1 = int.Parse(Num1Line);
+    int num2 = int.Parse(Num2Line);
+
+    //compare num1 and num2^2
+    if(num2 * num2 == num1)
+    // write result of comparison
+    {
+        Console.WriteLine("Первое число квадрат второго.");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не является квадратом второго.");
+    }
+}
