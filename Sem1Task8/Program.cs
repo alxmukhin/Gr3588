daa82069a1ed4a79f75cfbf8073e14b43ffc0dcf
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-03 01:05:10 +0300

    Comments added for Task 8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,25 +1,38 @@
 ﻿Console.WriteLine("Введите число:");
+//read number
 string? inputLine = Console.ReadLine();
+//null safety check
 if(inputLine!=null)
 {
+     //convert string into integer
      int number = int.Parse(inputLine);
+     //define first even value
      int index = 2;
+     //define string variable for storing output
      string outLine = string.Empty;
+     //check if number is even or odd
      if(number%2 == 0)
      {
+        //if number is even it will be last in output row, thus less than or equal operator is used
         while(index <= number)
         {
+            //start collecting even numbers in a row
             outLine = outLine + index + "  ";
+            //add two as only even values have to be recorded
             index = index + 2;
         }
      }
      else
      {
+         //if number is odd it should not be in output row, thus less than operator is used
          while(index < number)
         {
+            //start collecting even numbers in a row
             outLine = outLine + index + "  ";
+            //add two as only even values have to be recorded
             index = index + 2;
         }
      }
+     //output row with even numbers
      Console.WriteLine(outLine);
 }
