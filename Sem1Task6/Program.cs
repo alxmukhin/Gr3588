267b1e007e257c177dd05db80ff0e179ec9a74d8
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-02 22:58:04 +0300

    Task 6 completed

--- Sem1Task6/Program.cs
+++ Sem1Task6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+string? inputLine = Console.ReadLine();
+if(inputLine!=null)
+{
+    int number = int.Parse(inputLine);
+    int remainder = number%2;
+    if (remainder==0)
+    {
+        Console.WriteLine("Данное число является четным.");
+    }
+    else
+    {
+        Console.WriteLine("Данное число не является четным.");
+    }
+}
