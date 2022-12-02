e7ac6df0c4b3f9d3ce4df971d839d911c21454fd
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-02 19:52:36 +0300

    Task 2 completed

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿string? inputLine1 = Console.ReadLine();
+string? inputLine2 = Console.ReadLine();
+if(inputLine1!=null && inputLine2!=null)
+{
+    int Number1 = int.Parse(inputLine1);
+    int Number2 = int.Parse(inputLine2);
+    if(Number1 > Number2)
+    {
+        Console.WriteLine("Число " + Number1 + " больше числа " + Number2);
+    }
+    else
+    {
+        if(Number1 == Number2)
+        {
+            Console.WriteLine("Число " + Number2 + " равно числу " + Number1);
+        }
+        else
+        {
+            Console.WriteLine("Число " + Number2 + " больше числа " + Number1);
+        }
+    }
+}
