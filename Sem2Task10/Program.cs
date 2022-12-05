b7f3f23803f1543714501af2c82d6c3df105fa9d
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-05 22:15:19 +0300

    Task 10 completed, comments added

--- Sem2Task10/Program.cs
+++ Sem2Task10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число:");
+// read number string & convert to integer & null safety check
+int numberN = int.Parse(Console.ReadLine()??"0");
+//create variable to store result
+int result = new();
+//check if read number consists of three digits
+if(numberN>99 && numberN<1000)
+{
+    //extract second and third digits
+    result = numberN%100;
+    //extract second digit
+    result = result/10;
+    //output result
+    Console.WriteLine("Вторая цифра в данном числе: " + result);
+}
+else
+{
+    //output if read number does not consist of three digits
+    Console.WriteLine("Введенное число не является трехзначным! ");
+}
