f7c7bb49afd5eb4586ac81d535df31236deae2b6
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-05 23:01:24 +0300

    Task 13 completed

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -4,11 +4,11 @@ int numberN = int.Parse(Console.ReadLine()??"0");
 //create variable to store result
 char[] digits = numberN.ToString().ToCharArray();
 int numberLegth = digits.Length;
-if(numberLegth<2)
+if(numberLegth>2)
 {
-    Console.WrileLine("Третья цифра в данном числе:" + digits[2];)
+    Console.WriteLine("Третья цифра в данном числе: " + digits[2]);
 }
 else
 {
-    Console.WriteLine("В данном числе нет третьей цифры!")
+    Console.WriteLine("В данном числе нет третьей цифры!");
 }
