3f31fff5b360d1eb59cf0cc47c12747a984cee6c
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-05 23:05:53 +0300

    Comments added for Task 13

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,14 +1,18 @@
 ﻿Console.WriteLine("Введите число:");
 // read number string & convert to integer & null safety check
 int numberN = int.Parse(Console.ReadLine()??"0");
-//create variable to store result
+//convert integer into string and then to arrray
 char[] digits = numberN.ToString().ToCharArray();
+//define number of elements in array
 int numberLegth = digits.Length;
+//check if number consists of 3 digits and more
 if(numberLegth>2)
 {
+    //output result, thirds digit is stored as second element of array as their count starts from 0
     Console.WriteLine("Третья цифра в данном числе: " + digits[2]);
 }
 else
 {
+    //output message saying that number does not have third digit
     Console.WriteLine("В данном числе нет третьей цифры!");
 }
