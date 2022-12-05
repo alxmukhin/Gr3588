b1903f352a26639ea0f9f5ff13f636d07992ee6b
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-05 22:42:18 +0300

    Task 13 started

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+// read number string & convert to integer & null safety check
+int numberN = int.Parse(Console.ReadLine()??"0");
+//create variable to store result
+char[] digits = numberN.ToString().ToCharArray();
+int numberLegth = digits.Length;
+if(numberLegth<2)
+{
+    Console.WrileLine("Третья цифра в данном числе:" + digits[2];)
+}
+else
+{
+    Console.WriteLine("В данном числе нет третьей цифры!")
+}
