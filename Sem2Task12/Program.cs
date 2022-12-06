0b6b575d61ef997e229b24f800b394fc663e0a6e
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-06 18:57:54 +0300

    Task 12 completed

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,13 @@
+﻿int NumberA = int.Parse(Console.ReadLine()??"0");
+int NumberB = int.Parse(Console.ReadLine()??"0");
+
+int rest = NumberB%NumberA;
+
+if(rest==0)
+{
+    Console.WriteLine("Является кратным");
+}
+else
+{
+    Console.WriteLine("Не вляется кратным");
+}
