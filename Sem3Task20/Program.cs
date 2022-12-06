341a948b673401e78d9d73d548f013c27ff13548
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-06 22:43:34 +0300

    Task 20 completed

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int coordX1 = ReadData("Введите координату Х1:");
+int coordY1 = ReadData("Введите координату Y2:");
+int coordX2 = ReadData("Введите координату Х1:");
+int coordY2 = ReadData("Введите координату Y2:");
+
+double CalculateLength (int x1, int x2, int y1, int y2)
+{
+    double distance = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    return distance;
+}
+
+void PrintData (string msg, double dist)
+{
+    Console.WriteLine(msg + dist);
+}
+
+PrintData("Расстояние между точками равно: ", CalculateLength(coordX1, coordX2, coordY1, coordY2));
