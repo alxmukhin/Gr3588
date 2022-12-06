61de3808ca3d462c21b274404bb59e1c046e1948
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-06 18:59:33 +0300

    Task 14 completed

---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿int Number = int.Parse(Console.ReadLine()??"0");
+
+int result1 = Number%7;
+int result2 = Number%23;
+
+if(result1==0 && result2==0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
