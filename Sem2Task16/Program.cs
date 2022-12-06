a4806dd05db3e40734eae67cf5b418c5f0d0acd5
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-06 19:01:14 +0300

    Task 16 completed

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿void TestSQRT(int i, int j)
+{
+    bool result = (i/j==j);
+    if(result==true) Console.WriteLine("Число " + i + " является квадратом числа " + j);
+    else Console.WriteLine("Число " + i + " не является квадратом числа " + j);
+}
+
+int NumberA = int.Parse(Console.ReadLine()??"0");
+int NumberB = int.Parse(Console.ReadLine()??"0");
+TestSQRT(NumberA, NumberB);
+TestSQRT(NumberB, NumberA);
