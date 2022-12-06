1d15db86935f1d050f536fa516d89d3752ddc18b
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-06 22:43:57 +0300

    Task 22 completed

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,27 @@
+﻿int numberN = ReadData("Введите число N: ");
+string outline1 = LineBuilder(numberN, 1);
+PrintResult("Числа от 1 до N: ", outline1);
+string outline2 = LineBuilder(numberN, 2);
+PrintResult("Квадраты чисел от 1 до N: ", outline2);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+string LineBuilder(int num, int pow)
+{
+    string line = string.Empty;
+    for (int i=1; i<num; i++)
+    {
+        line = line + " " + Math.Pow(i, pow);
+    }
+    line = line + " " + Math.Pow(num, pow);
+    return line;
+}
+
+void PrintResult(string msg, string line)
+{
+    Console.WriteLine(msg + line);
+}
