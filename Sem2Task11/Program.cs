6c61f60e24e128cf2d4dc3631ca9fbed0966df0d
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-06 18:56:22 +0300

    Task 11 completed

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,10 @@
+﻿//Создаем экземпляр класса рандомайзера
+System.Random numberSintezator = new System.Random();
+
+//Получаем новое случайное число
+int number = numberSintezator.Next(100,1000);
+
+Console.WriteLine(number);
+
+int result = ((number/100)*10 + number%10);
+Console.WriteLine(result);
