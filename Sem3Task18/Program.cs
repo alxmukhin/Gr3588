c0d36fc0cfa242e5db1e92c1f746f9dcf56e4c15
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-06 22:43:11 +0300

    Task 18 completed

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите номер четверти:");
+int quarter = int.Parse(Console.ReadLine() ?? "0");
+
+void XYrange(int num)
+{
+    if (num > 0 && num < 5)
+    {
+        if (num == 1) Console.WriteLine("X>0 & Y>0");
+        if (num == 2) Console.WriteLine("X>0 & Y<0");
+        if (num == 3) Console.WriteLine("X<0 & Y<0");
+        if (num == 4) Console.WriteLine("X<0 & Y>0");
+    }
+    else
+    {
+        Console.WriteLine("Это не номер четверти!");
+    }
+}
+
+XYrange(quarter);
