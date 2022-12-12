c650cbd7859dc3db5927ff23dc2b35a5b1a07bc5
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-13 00:44:39 +0300

    Task 19 completed

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,40 @@
+﻿int number = ReadData("Введите пятизначное число:");
+PrintData(number, PolyndromTest(number));
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+bool PolyndromTest (int number)
+{
+    bool testResult = false;
+    if(number%10 == number/10000 && number%100/10 == number/1000%10)
+    {
+        testResult = true;
+    }
+    else
+    {
+        testResult = false;
+    }
+    return testResult;
+}
+void PrintData (int number, bool testResult)
+{
+    if(Math.Log10(number)>3 && Math.Log10(number)<5)
+    {
+        if(testResult == true)
+        {
+            Console.WriteLine("Данное число является полиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Данное число не является полиндромом.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не является пятизначным!");
+    }
+}
