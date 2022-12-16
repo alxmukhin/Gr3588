db23cc0c8f48f6385e0a3e137c69c640253fcf83
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-16 19:31:30 +0300

    Task 29 completed

---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,23 @@
+﻿string line = ReadLine("Введите перечень имен, разделяя их запятой:");
+PrintResult("Случайное имя из данного перечня: ", RandomName(line));
+
+string ReadLine(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine() ?? "0";
+}
+
+string RandomName(string line)
+{
+    string[] names = line.Split(',');
+    int lenght = names.Length;
+    Random rnd = new Random();
+    int randnum = rnd.Next(0, lenght+1);
+    string randname = names[randnum];
+    return randname;
+}
+
+void PrintResult(string message, string randname)
+{
+    Console.WriteLine(message + randname);
+}
