a014039f72cfaf48cde8f4ffb447ce9530e676ad
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-20 19:52:35 +0300

    Comments added for Task 38

--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -1,17 +1,25 @@
-﻿int number = ReadData("Введите число элементов массива:");
+﻿//collect number of elements in array
+int number = ReadData("Введите число элементов массива:");
+//generate array of double based on given conditions
 double[] arrayInitial = GenerateDoubleArray(number);
+//output of generated array
 PrintArray("В заданном массиве:", arrayInitial);
-PrintResultEven("Разница между максимальным и минимальным элементами равна: ", MaxMinDiff(arrayInitial));
+//output difference between max and min elements
+PrintResultDiffMaxMin("Разница между максимальным и минимальным элементами равна: ", MaxMinDiff(arrayInitial));
 
+//ReadData method - collect input data
 int ReadData(string message)
 {
     Console.WriteLine(message);
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
+//GenerateArray method creates new double array based on given conditions
 double[] GenerateDoubleArray(int length)
 {
+     //create new array
     double[] array = new double[length];
+    //fill in new array with element values from 0 to 1
     for (int i = 0; i < length; i++)
     {
         Random rnd = new Random();
@@ -20,40 +28,52 @@ double[] GenerateDoubleArray(int length)
     return array;
 }
 
+//MaxMinDiff calculated difference between max and min elements value
 double MaxMinDiff(double[] array)
 {
+    //assign max value for search
     double max = double.MinValue;
+    //assign min value for search
     double min = double.MaxValue;
+    //cycle for search max and min values
     for (int i = 0; i < array.Length; i++)
     {
+        //condition for max value
         if (max < array[i])
         {
             max = array[i];
         }
+        //condition for min value
         if (min > array[i])
         {
             min = array[i];
         }
     }
-    Console.WriteLine(min);
-    Console.WriteLine(max);
+    //calculate difference between min and max
     double diff = max - min;
     return diff;
 }
 
+//PrintArray method used to represent array as a string
 void PrintArray(string message, double[] array)
 {
+    //define number of elements in array
     int length = array.Length;
+    //create new string
     string arrayLine = string.Empty;
+    //build string by adding array element on each step
     for (int i = 0; i < length; i++)
     {
         arrayLine = arrayLine + array[i] + " ";
     }
+    //output string
     Console.WriteLine(message);
+    //output string containing array
     Console.WriteLine(arrayLine);
 }
 
-void PrintResultEven(string message, double diff)
+//PrintResultDiffMaxMin method prints difference between min and max elements
+void PrintResultDiffMaxMin(string message, double diff)
 {
     Console.WriteLine(message + diff);
 }
