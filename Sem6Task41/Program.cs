1de0dfc9c22b84fe5b9f53e9e6e4025cbfd45d20
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-23 14:15:32 +0300

    Task 41 completed

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿int number = ReadData("Введите количество чисел: ");
+PrintResult("Количество чисел больше нуля среди введенных: ", CountPositive(number));
+
+
+//ReadData method requests user to input data and collects it
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//CountPositive method counts nquantity of positive values inserted by user
+int CountPositive (int number)
+{
+    int i = 0;
+    //crreate variable to store result of counting
+    int counter = 0;
+    //laubch counting cycle based on value inserted by user
+    while (i < number)
+    {
+        int inputNumber = ReadData("Введите число " + (i + 1) + ": ");
+        if (inputNumber > 0)
+        {
+            counter = counter + 1;
+        }
+        i++;
+    }
+    return counter;
+}
+
+//PrintResult method outputs result with certain message
+void PrintResult(string message, int number)
+{
+    Console.WriteLine(message + number);
+}
