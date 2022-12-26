c9e55591ad38e9b8ffb9d2f327b18be63fce4751
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-26 23:08:45 +0300

    Comments added for task 47

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿int m = ReadData("Введите число строк: ");
+int n = ReadData("Введите число столбцов: ");
+PrintArray(FillArrayRandom(m, n));
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+double[,] FillArrayRandom(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            array[i, j] = new System.Random().NextDouble() * 100;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write("{0:N2}  ", array[i, j]);
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
