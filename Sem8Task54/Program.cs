e408829aa81c56dae43727e8b0e0e1dc9fe2a38e
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-30 13:35:57 +0300

    Task 54 completed

---
+++ Sem8Task54/Program.cs
@@ -0,0 +1,69 @@
+﻿//get number of rows
+int rows = ReadData("Введите число строк: ");
+//get number of columns
+int columns = ReadData("Введите число столбцов: ");
+//fill 2D array with random values
+int[,] array = FillArrayRandom(rows, columns);
+//print initial array
+PrintArray("Массив с заданными параметрами:", array);
+//print sorted array
+PrintArray("Массив с элементами отсортированными в строках по убыванию:", SortArrayDecrease(array));
+
+
+//ReadData method requests user to insert data and collects it
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//FillArrayRanom fills 2D array with random integer
+int[,] FillArrayRandom(int rows, int columns)
+{
+    //create new array as per inserted parameters
+    int[,] array = new int[rows, columns];
+    //cycle of filling array
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i, j] = new System.Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+//PrintArray method for pint out array
+void PrintArray(string message, int[,] array)
+{
+    Console.WriteLine(message);
+    //cycle of array printing
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("  {0}   ", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArrayDecrease(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++ )
+    {
+        for(int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for(int k = 0; k < array.GetLength(1) - j - 1; k++)
+            {
+                if(array[i, k + 1] > array[i, k])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i,  k+ 1] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
