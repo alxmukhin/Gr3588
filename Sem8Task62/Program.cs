2313b4c8eea055245e7bd32cc772f28467b1a7cd
Aleksandr Mukhin <alxmukhin@gmail.com>
2022-12-30 23:02:59 +0300

    Task 62 updated

--- Sem8Task62/Program.cs
+++ Sem8Task62/Program.cs
@@ -3,14 +3,14 @@
 int[,] SpiralFill()
 {
     int[,] spiralArray = new int[4, 4];
-    spiralArray[0,0] = 1;
+    spiralArray[0, 0] = 1;
     for(int i = 0; i < 3; i++)
     {
-        spiralArray[0, i+1] = spiralArray[0, i] + 1;
+        spiralArray[0, i + 1] = spiralArray[0, i] + 1;
     }
     for(int j = 0; j < 3; j++)
     {
-        spiralArray[j+1, 3] = spiralArray[j,3] + 1;
+        spiralArray[j + 1, 3] = spiralArray[j, 3] + 1;
     }
     for(int k = 3; k > 0; k--)
     {
@@ -18,19 +18,19 @@ int[,] SpiralFill()
     }
      for(int l = 3; l > 1; l--)
     {
-        spiralArray[l-1, 0] = spiralArray[l,0] + 1;
+        spiralArray[l - 1, 0] = spiralArray[l, 0] + 1;
     }
      for(int m = 0; m < 2; m++)
     {
-        spiralArray[1, m+1] = spiralArray[1, m] + 1;
+        spiralArray[1, m + 1] = spiralArray[1, m] + 1;
     }
        for(int n = 1; n < 2; n++)
     {
-        spiralArray[n+1, 2] = spiralArray[n,2] + 1;
+        spiralArray[n + 1, 2] = spiralArray[n, 2] + 1;
     }
       for(int o = 2; o > 1; o--)
     {
-        spiralArray[2, o-1] = spiralArray[2, o] + 1;
+        spiralArray[2, o - 1] = spiralArray[2, o] + 1;
     }
     return spiralArray;
 }
@@ -42,7 +42,7 @@ void PrintArray(int[,] array)
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(" {00} ", array[i, j]);
+            Console.Write("{0} ", array[i, j]);
         }
         Console.WriteLine();
     }
