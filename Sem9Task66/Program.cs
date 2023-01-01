0248d3112447d20034aac315a43c147844153707
Aleksandr Mukhin <alxmukhin@gmail.com>
2023-01-01 17:26:29 +0300

    Task 66 completed

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿int numberM = ReasData("Введите число M:");
+int numberN = ReasData("Введите число N:");
+PrintResult("Сумма чисел от M до N равна: ", RecursionSum(numberM, numberN));
+
+int ReasData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int RecursionSum(int numberM, int numberN)
+{
+    if(numberM >= numberN)
+    {
+        return numberM;
+    }
+    else
+    {
+        return numberM + RecursionSum(numberM + 1, numberN);
+    }
+}
+
+void PrintResult(string message, int sum)
+{
+    Console.WriteLine(message + sum);
+}
