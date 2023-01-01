d3f862306b6bc329012273a8117b8580e5d0c6e0
Aleksandr Mukhin <alxmukhin@gmail.com>
2023-01-01 13:14:40 +0300

    Task 64 completed

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿int number = ReasData("Введите число N:");
+PrintResult("Последовательность натуральных чисел от N до 1: ", RecursionDecrease(number));
+
+int ReasData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+string RecursionDecrease(int number)
+{
+    if(number <= 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        string result = number + " " + RecursionDecrease(number - 1);
+        return result;
+    }
+}
+
+void PrintResult(string message, string result)
+{
+    Console.WriteLine(message + result);
+}
