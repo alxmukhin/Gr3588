a72cbb15efbd5ac93e6c7f5a7b877ae2523a7ea2
Aleksandr Mukhin <alxmukhin@gmail.com>
2023-01-01 19:27:06 +0300

    Comments added for task 68

--- Sem9Task68/Program.cs
+++ Sem9Task68/Program.cs
@@ -1,15 +1,21 @@
-﻿int m = ReasData("Введите аргумент m функции Аккермана:");
-int n = ReasData("Введите аргумент n функции Аккермана:");
-PrintResult("Значение функции Аккермана: ", RecursionAckermann(m, n));
+﻿//get parameter "m" for Ackermann function
+int m = ReasData("Введите параметр m функции Аккермана:");
+//get parameter "n" for Ackermann function
+int n = ReasData("Введите параметр n функции Аккермана:");
+//calculate value of Ackermann function for given parameters and print it
+PrintResult("Значение функции Аккермана для заданных параметров равно: ", RecursionAckermann(m, n));
 
+//ReadData method requests user to insert data and collects it
 int ReasData(string message)
 {
     Console.WriteLine(message);
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
+//RecursionAckerman method receives "m" and "n" parameters and calculate value of Ackermann function for them
 int RecursionAckermann(int m, int n)
 {
+    //calculation of Ackermann function based on its definition
     if(m > 0)
     {
         if(n > 0)
@@ -28,6 +34,7 @@ int RecursionAckermann(int m, int n)
 
 }
 
+//PrintResult method prints value of Ackermann function calculated by recursion with explanation message prior to it
 void PrintResult(string message, int ackerman)
 {
     Console.WriteLine(message + ackerman);
