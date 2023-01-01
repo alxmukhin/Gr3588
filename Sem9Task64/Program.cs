120d54651abc2a6db7a33bdc7319c30bc960b1ff
Aleksandr Mukhin <alxmukhin@gmail.com>
2023-01-01 18:14:15 +0300

    Comments added for task 64

--- Sem9Task64/Program.cs
+++ Sem9Task64/Program.cs
@@ -1,18 +1,24 @@
-﻿int number = ReasData("Введите число N:");
+﻿//get value of N
+int number = ReadData("Введите число N:");
+//create string of values from N to 1 and print it
 PrintResult("Последовательность натуральных чисел от N до 1: ", RecursionDecrease(number));
 
-int ReasData(string message)
+//ReadData method requests user to insert data
+int ReadData(string message)
 {
     Console.WriteLine(message);
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
+//RecursionDecrease method creates string of values from number to 1
 string RecursionDecrease(int number)
 {
+    //condition for stop of recusion
     if(number <= 0)
     {
         return string.Empty;
     }
+    //use recursion to creates string
     else
     {
         string result = number + " " + RecursionDecrease(number - 1);
@@ -20,6 +26,7 @@ string RecursionDecrease(int number)
     }
 }
 
+//PrintResult method prints string created by recursion with explanation message prior to it
 void PrintResult(string message, string result)
 {
     Console.WriteLine(message + result);
